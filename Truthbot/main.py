958aa85b4bea2bd098297e98fc574205d8887e37
oeilsimple <Oeilsimple@gmail.com>
2025-12-21 23:01:53 +0100

    fixing func calling

--- Truthbot/main.py
+++ Truthbot/main.py
@@ -1,8 +1,7 @@
 from azure.ai.projects import AIProjectClient 
 from azure.identity import DefaultAzureCredential
 from azure.ai.agents.models import ListSortOrder  
-from utils import tavily_web_search
-from call_agent import run_agent_and_get_response, project, agent
+from utils import tavily_web_search ,run_agent_and_get_response, project, agent
 import os
 from dotenv import load_dotenv
 
